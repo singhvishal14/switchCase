a47e94f7de15d7c4b52871eccd8c621993870294
singhvishal14 <kumarvishalsingh767@gmail.com>
2023-07-11 11:25:32 +0530

    third commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cCode,lang,Choice;
+            do { 
+            Console.WriteLine("Enter Country Code");
+            cCode = Console.ReadLine();
+            switch(cCode)
+            {
+                case "uk":
+                case "us":
+                    {
+                        lang = "English";
+                        break;
+                    }
+                case "af":
+                    {
+                        lang = "Dari,Pashto";
+                        break;
+                    }
+                case "in":
+                    { 
+                        lang = "Hindi,English,Tamil and Telgu";
+                        break;
+                    }
+                case "uae":
+                case "om":
+                case "ksa":
+                case "egypt":
+                    {
+                        lang = "Arabic";
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine($"Sorry!! Your Country {cCode} is not found");
+                        lang = "Not Found!!!!!!!";
+                        break;
+                    }
+            }
+            Console.WriteLine("Country Code: \t " + cCode + "\n Language(s): \t"  + lang);
+            Console.WriteLine("Do you wanna continue if yes press y \n To exit press any key");
+            Choice = Console.ReadLine().ToLower(); 
+            
+        }
+        while (Choice == "y");
+    }
+}
+}
